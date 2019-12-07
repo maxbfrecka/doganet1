108007dc707ae97736eafe122678d831df5c6839
root <maxbfrecka@gmail.com>
2019-12-07 18:05:11 +0000

    test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Doga Media
                 </div>
 
                 <div class="links">
